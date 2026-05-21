30665c7d4c0e54d64684db74fe7702f5899e13da
aryasheetal9934-cell/DSA <aryasheetal9934@gmail.com>
2026-05-21 22:07:48 +0530

    notes

---
+++ lecture 2/variables.py
@@ -0,0 +1,13 @@
+#camel case
+myFirstName="sheetal"
+mySecondName="Arya"
+
+#pascal case
+MyFirstName="sheetal"
+MySecondName="Arya"
+
+#snake case
+my_first_name="sheetal"
+
+#smaller case
+my_first_name="sheetal"
