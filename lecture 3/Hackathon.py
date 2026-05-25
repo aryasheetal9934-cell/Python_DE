be8a4f01843189881517a6e24638ea0ad0c69b81
aryasheetal9934-cell/DSA <aryasheetal9934@gmail.com>
2026-05-25 18:45:39 +0530

    notes

---
+++ lecture 3/Hackathon.py
@@ -0,0 +1,7 @@
+friends=["aham","sheetal","nikhil","rudra"]
+gender=["male","female","male","male"]
+if 'female' in gender:
+ print("there is atleat one female in the list.hence , group is allowed for hackthon")
+
+else:
+  print("there are no female in the list.hence,group is not allowed for hackthon")
