5540e9cfb4c113e0e9741b4fd1a8ce82aef777b6
aryasheetal9934-cell/DSA <aryasheetal9934@gmail.com>
2026-05-25 18:28:49 +0530

    notes

---
+++ lecture 4/pattern.py
@@ -0,0 +1,19 @@
+# n=5
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(i,end=" ")
+#     print()
+
+# for i in range(1,6):
+#     print(str(i)*i)
+
+# for i in range(1,6):
+#     print((10**i//9)*i)
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print(i,end="")
+#     print()
+
+for i in range (1,5):
+    print("*")
