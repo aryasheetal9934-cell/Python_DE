736f92328f7e03a996d454e1c607169636353ae8
aryasheetal9934-cell/DSA <aryasheetal9934@gmail.com>
2026-05-25 11:48:44 +0530

    notes

--- lecture 4/prime_num.py
+++ lecture 4/prime_num.py
@@ -1,13 +1,10 @@
-nums=[2,3,5,7,11,13,19,20]
+list=[2,3,5,7,11,13,19,20]
 
-for num in nums:
-     if num>1:
-        for x in range(2,num):
-           if num%x==0:
-            print(num,"not prime")
-           break
-
+for num in list:
+    if num>1:
+        for i in range(2,num):
+            if num%i==0:
+                break
         else:
-           print(num,"prime")    
-
+         print(num)
 
