ed36768007f5119277d378f8bc2cdbe56aee1130
aryasheetal9934-cell/DSA <aryasheetal9934@gmail.com>
2026-05-27 21:27:05 +0530

    notes

---
+++ lecture 6/tuple.py
@@ -0,0 +1,15 @@
+tup=tuple("sheetal")
+print(tup)
+
+# convert str into tuple
+str="sheetal"
+print(str)
+str1=tuple(str)
+print(str1)
+
+print(tup[0])
+print(tup[1:4])
+print(len(tup))
+print(tup.count("e"))
+print(tup[::-1])
+
