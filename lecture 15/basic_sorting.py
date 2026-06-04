0d50d0f6ac841d6d3e96a32f5244b7131c769f48
aryasheetal9934-cell/DSA <aryasheetal9934@gmail.com>
2026-06-04 19:14:05 +0530

    notes

---
+++ lecture 15/basic_sorting.py
@@ -0,0 +1,15 @@
+# a=sorted([5,2,8,1,9])   ## Ascending order
+# print(a)
+
+# print(sorted([5,2,8,1,9],reverse=True))  ## Descending order
+
+# a=[5,2,8,1,9]
+# a.sort() ## ascending order
+# print(a)
+
+# str1=sorted("This is a string".split(),key=str.casefold)
+# print(str1)
+
+dict={1:"one",3:"three",2:"two"}
+sorted_dict=sorted(dict.items())
+print(sorted_dict)
