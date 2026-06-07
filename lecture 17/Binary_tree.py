8a04cd9315fe6d7b05f86dc6bc68c560622c99fa
aryasheetal9934-cell/DSA <aryasheetal9934@gmail.com>
2026-06-07 10:52:07 +0530

    notes

--- lecture 17/Binary_tree.py
+++ lecture 17/Binary_tree.py
@@ -20,5 +20,26 @@ nodeA.right = nodeD
 nodeB.left = nodeE
 nodeB.right = nodeF 
 root.left = nodeG
-print("Data of root.right.left:", root.right.left.value)
-print("Data of root.left.value:", root.left.value)
+# print("Data of root.right.left:", root.right.left.value)
+# print("Data of root.left.value:", root.left.value)
+
+def preOrder(node): ### Root Left Right
+    if node is None:
+        return 
+    print(node.value,end=",")
+    preOrder(node.left)
+    preOrder(node.right)
+
+def inOrder(node):  ### Left Root Right
+    if node is None:
+        return
+    inOrder(node.left)
+    print(node.value, end=",")
+    inOrder(node.right)
+
+def postOrder(node): ### Left Right Root
+    if node is None:
+        return
+    postOrder(node.left)
+    postOrder(node.right)
+    print(node.value, end=",")
